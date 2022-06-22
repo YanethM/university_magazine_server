8938667f4229c38857315fd0244c580ae3058074
YanethM <yaneth.mejiar@autonoma.edu.co>
2022-06-22 13:36:14 -0500

    ruta carpeta uploads

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -112,6 +112,7 @@ const activateUser = (req, res) => {
 
 function uploadAvatar(req, res) {
   const params = req.params;
+  console.log("uploadAvatar");
   User.findById({ _id: params.id }, (err, userData) => {
     if (err) {
       res.status(500).send({ message: "Error del servidor." });
@@ -125,13 +126,14 @@ function uploadAvatar(req, res) {
           let filePath = req.files.avatar.path;
           let fileSplit = filePath.split("/");
           let fileName = fileSplit[2];
+
           let extSplit = fileName.split(".");
           let fileExt = extSplit[1];
 
           if (fileExt !== "png" && fileExt !== "jpg") {
             res.status(400).send({
               message:
-                "La extension de la imagen no es valida. (Extensiones permitidas: .png y .jpg)",
+                "La extension de la imagen no es valida. (Extensiones permitidas: .png y .jpg)"
             });
           } else {
             user.avatar = fileName;
@@ -161,9 +163,9 @@ function uploadAvatar(req, res) {
 
 function getAvatar(req, res) {
   const avatarName = req.params.avatarName;
-  const filePath = "./assets/avatar" + avatarName;
+  const filePath = "./uploads/avatar/" + avatarName;
 
-  fs.exists(filePath, (exists) => {
+  fs.exists(filePath, exists => {
     if (!exists) {
       res.status(404).send({ message: "El avatar que buscas no existe." });
     } else {
@@ -171,6 +173,7 @@ function getAvatar(req, res) {
     }
   });
 }
+
 
 async function updateUser(req, res) {
   let userData = req.body;
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,8 @@ const express = require("express");
 const UserController = require("../controllers/user");
 const multipart = require("connect-multiparty");
 const middleware_user_authenticated = require("../middleware/authenticated_user");
-const md_upload_avatar = multipart({ uploadDir: "./assets/avatar" });
+const md_upload_avatar = multipart({ uploadDir: "./uploads/avatar" });
+
 
 const api = express.Router();
 
@@ -19,30 +20,22 @@ api.get(
   [middleware_user_authenticated.ensureAuth],
   UserController.getActiveUsers
 );
-api.put(
-  "/uploadavatar/:id",
-  [middleware_user_authenticated.ensureAuth, md_upload_avatar],
-  UserController.uploadAvatar
-);
-api.put(
-  "/updateuser/:id",
-  [middleware_user_authenticated.ensureAuth],
-  UserController.updateUser
-);
-api.put(
-  "/activateuser/:id",
-  [middleware_user_authenticated.ensureAuth],
-  UserController.activateUser
-);
-api.delete(
-  "/deleteuser/:id",
-  [middleware_user_authenticated.ensureAuth],
-  UserController.deleteUser
-);
-api.post(
-  "/signupadmin",
-  [middleware_user_authenticated.ensureAuth],
-  UserController.signUpAdmin
-);
+
+  api.put(
+    "/uploadavatar/:id",
+    [middleware_user_authenticated.ensureAuth, md_upload_avatar],
+    UserController.uploadAvatar
+  );
+  api.get("/getavatar/:avatarName", UserController.getAvatar);
+  api.put("/updateuser/:id", [middleware_user_authenticated.ensureAuth], UserController.updateUser);
+  api.put(
+    "/activateuser/:id",
+    [middleware_user_authenticated.ensureAuth],
+    UserController.activateUser
+  );
+  api.delete("/deleteuser/:id", [middleware_user_authenticated.ensureAuth], UserController.deleteUser);
+  api.post("/signupadmin", [middleware_user_authenticated.ensureAuth], UserController.signUpAdmin);
+
+
 
 module.exports = api;
